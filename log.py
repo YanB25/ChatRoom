6e3fa6d12f69f3b36ab70273b47719f13af0a641
YanB25 <442826556@qq.con>
2018-01-31 18:29:53 +0800

    finishe server

---
+++ log.py
@@ -0,0 +1,22 @@
+import json
+VERBOSE = 0
+WARNING = 1
+ERROR = 2
+
+def log(msg, level):
+    '''
+    used to print log imformation
+    '''
+    if level == -1:
+        setting_json = open('setting.json', 'r').read()
+        json_data = json.loads(setting_json)
+        log_level_msg = json_data['logLevel']
+        if log_level_msg == "VERBOSE":
+            log.level = VERBOSE
+        elif log_level_msg == "WARNING":
+            log.level = WARNING
+        else:
+            log.level = ERROR
+    if level >= log.level:
+        print(msg)
+log.level = -1
---
+++ server.py
@@ -0,0 +1,63 @@
+import sys
+import select
+import socket
+import log
+
+class ChatRoom(object):
+    '''
+    main class for server
+    usage:
+    chatRoom = ChatRoom()
+    chatRoom.run(<port>)
+    '''
+    def __init__(self, port):
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind(('', self.port))
+        self.socket.listen(10)
+        self.descriptors = [self.socket]
+        log.log("init socket in port " + str(port), log.VERBOSE)
+    
+    def run(self):
+        log.log("start running ...", log.VERBOSE)
+        while True:
+            readList, _, _ = select.select(self.descriptors, [], [], 2)
+            if readList:
+                for sock in readList:
+                    if sock == self.socket:
+                        # new connection is joining
+                        self.add_new_connection()
+                    else:
+                        msg = sock.recv(1024)
+                        host, port = sock.getpeername()
+                        if (msg):
+                            # broadcast
+                            self.broadcase_message(msg)
+                        else:
+                            # disconnect
+                            bs = "{}:{} disconnect".format(str(host), str(port))
+                            self.broadcase_message(bs)
+                            sock.close()
+                            self.descriptors.remove(socket)
+    
+    def add_new_connection(self):
+        connectionSocket, addr = self.socket.accept()
+        self.descriptors.append(connectionSocket)
+        bs = "{} join the chat room".format(str(addr))
+        self.broadcase_message(bs)
+        
+    def broadcase_message(self, msg):
+        log.log("broadcasting {}".format(msg), log.VERBOSE)
+        for sock in self.descriptors:
+            if (sock != self.socket):
+                sock.send(msg)
+
+if __name__ == "__main__":
+    chatRoom = ChatRoom(12000)
+    chatRoom.run()
+
+
+
+
+
+
